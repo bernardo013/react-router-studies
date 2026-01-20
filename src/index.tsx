ce1b959d0495c0cb3d2f57df95620489b45ed828
Bernardo Marques <japatvs1@gmail.com>
2026-01-20 09:16:56 -0300

    iniciando nas rotas

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,11 @@ import './index.css';
 import Cardapio from './pages/Cardapio';
 import Inicio from 'pages/Inicio';
 
+const componenteAtual = window.location.pathname === '/' ? <Inicio></Inicio> : <Cardapio></Cardapio>;
+
 ReactDOM.render(
   <React.StrictMode>
-    <Inicio />
+    {componenteAtual}
   </React.StrictMode>,
   document.getElementById('root')
 );
