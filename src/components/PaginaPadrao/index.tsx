a3838e624d4b1a1253df56242536214bfb5050e8
bernardo013 <105383046+bernardo013@users.noreply.github.com>
2026-01-22 09:55:13 -0300

    Criação do sobre e Footer, incluindo rotas

--- src/components/PaginaPadrao/index.tsx
+++ src/components/PaginaPadrao/index.tsx
@@ -1,6 +1,7 @@
 import styles from './PaginaPadrao.module.scss';
 //Childen do react-router-dom
 import { Outlet } from 'react-router-dom';
+import stylesTema from 'styles/Tema.module.scss';
 
 export default function PaginaPadrao() {
   return (
@@ -10,7 +11,7 @@ export default function PaginaPadrao() {
           A casa do código e da massa
         </div>
       </header>
-      <div>
+      <div className={stylesTema.container}>
         {/* O <Outlet /> é onde o conteúdo variável aparece. */}
         <Outlet></Outlet>
       </div>
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
+import Footer from 'components/Footer';
 import Menu from 'components/Menu';
 import PaginaPadrao from 'components/PaginaPadrao';
 import Cardapio from 'pages/Cardapio';
+import Sobre from 'pages/Cardapio/Sobre';
 import Inicio from 'pages/Inicio';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -11,15 +13,17 @@ export default function AppRouter() {
       <Router>
         <Menu/>
 
-
+        {/* nested routes */}
         <Routes>
           {/* Representa a rota pai */}
           <Route path='/' element={<PaginaPadrao/>}>
             {/* Quando a rota for exatamente / ... Renderize o componente Inicio */}
             <Route index element={<Inicio />} />
             <Route path='cardapio' element={<Cardapio/>} /> 
+            <Route path='sobre' element={<Sobre />} />
           </Route>
         </Routes>
+        <Footer />
       </Router>
     </main>
   );
